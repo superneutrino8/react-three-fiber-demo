0de933e6b183f8c73f5ed8045e86d0254ddd7c35
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-10-19 17:54:38 +0530

    Add Soft Shadow and Shadow Mesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
+import { softShadows } from "drei";
 
 import "./App.scss";
+
+softShadows();
 
 const SpiningBox = ({ position, args, color }) => {
   const mesh = useRef(null);
@@ -9,7 +12,7 @@ const SpiningBox = ({ position, args, color }) => {
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh position={position} ref={mesh} castShadow>
       <boxBufferGeometry attach="geometry" args={args} />
       <meshStandardMaterial attach="material" color={color} />
     </mesh>
@@ -19,8 +22,38 @@ const SpiningBox = ({ position, args, color }) => {
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 75 }}>
+      <Canvas
+        shadowMap
+        colorManagement
+        camera={{ position: [-5, 2, 10], fov: 75 }}
+      >
         <ambientLight intensity={0.2} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+
+        <group>
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+        </group>
+
         <SpiningBox position={[0, 1, 0]} args={[3, 2, 1]} color="lightblue" />
         <SpiningBox position={[-2, 1, -5]} color="lightpink" />
         <SpiningBox position={[5, 1, -2]} color="lightpink" />
